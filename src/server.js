0ad24ebaa8fc4283460c16c0633789aa00c17e71
Gleider <gmdcampos@inf.ufpel.edu.br>
2020-08-01 22:42:23 -0300

    add middleware of errors

--- src/server.js
+++ src/server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const { config } = require('dotenv');
 const routes = require('./routes');
 const envConfig = require('../config/envConfig');
+const { generalErrors } = require('./middlewares/errors');
 
 config(envConfig);
 
 const port = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.json());
 app.use(routes);
-app.listen(5000, () => {
-  console.log(`Server running on port ${port}`);
-});
+
+app.use(generalErrors);
+
+app.listen(5000, () => console.log(`Server running on port ${port}`));
