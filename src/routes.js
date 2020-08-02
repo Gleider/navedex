a8ad85e24c73e49efae41989dba5b671886148e0
Gleider <gmdcampos@inf.ufpel.edu.br>
2020-08-02 18:57:01 -0300

    route to create project

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const AuthController = require('./controllers/AuthController');
 const UserController = require('./controllers/UserController');
 const NaverController = require('./controllers/NaverController');
+const ProjectController = require('./controllers/ProjectController');
 const authMiddleware = require('./middlewares/auth');
 
 const routes = express.Router();
@@ -11,11 +12,15 @@ routes.post('/login', AuthController.login);
 
 routes.use(authMiddleware);
 // all routes below need authorization
+// Navers
 routes.get('/navers', NaverController.index);
 routes.get('/navers/:id', NaverController.show);
 routes.post('/navers', NaverController.store);
 routes.put('/navers/:id', NaverController.update);
 routes.delete('/navers/:id', NaverController.delete);
+
+// Projects
+routes.post('/projects', ProjectController.store);
 
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.create);
