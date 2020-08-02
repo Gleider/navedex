186733d201f412f644b93864a394ce53861d5baa
Gleider <gmdcampos@inf.ufpel.edu.br>
2020-08-01 22:43:54 -0300

    created migration to users

--- src/database/migrations/20200731131737_create_users.js
+++ src/database/migrations/20200731131737_create_users.js
@@ -2,9 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('users', (t) => {
     t.uuid('id').primary().notNullable();
     t.string('email').unique().notNullable();
-    t.string('name');
-    t.string('password');
-    t.date('birthdate');
+    t.string('password').notNullable();
     t.timestamp('updated_at').defaultTo(knex.fn.now());
     t.timestamp('created_at').defaultTo(knex.fn.now());
   });
