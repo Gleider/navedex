14c1e6a4af72cfa7dedbe311770f0af42339fa42
Gleider <gmdcampos@inf.ufpel.edu.br>
2020-08-02 11:39:58 -0300

    create migration to navers projects

---
+++ src/database/migrations/20200802105804_create_navers_projects.js
@@ -0,0 +1,16 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('navers_projects', (t) => {
+    t.uuid('naver_id')
+      .references('navers.id')
+      .notNullable();
+    t.uuid('project_id')
+      .references('projects.id')
+      .notNullable();
+    t.timestamp('updated_at').defaultTo(knex.fn.now());
+    t.timestamp('created_at').defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('navers_projects');
+};
