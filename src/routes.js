0d65368585de11943701e87a6ac645351c157e44
Gleider <gmdcampos@inf.ufpel.edu.br>
2020-08-02 11:40:41 -0300

    add auth middleware

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const AuthController = require('./controllers/AuthController');
 const UserController = require('./controllers/UserController');
+const authMiddleware = require('./middlewares/auth');
 
 const routes = express.Router();
 
 routes.post('/signup', AuthController.signup);
 routes.post('/login', AuthController.login);
 
+routes.use(authMiddleware);
+// all routes below need authorization
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.create);
 
