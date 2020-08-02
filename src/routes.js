3d156adbbddc7efde017f8a94e15d8716101078a
Gleider <gmdcampos@inf.ufpel.edu.br>
2020-08-02 13:03:55 -0300

    route to create a naver

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const AuthController = require('./controllers/AuthController');
 const UserController = require('./controllers/UserController');
+const NaverController = require('./controllers/NaverController');
 const authMiddleware = require('./middlewares/auth');
 
 const routes = express.Router();
@@ -10,6 +11,8 @@ routes.post('/login', AuthController.login);
 
 routes.use(authMiddleware);
 // all routes below need authorization
+routes.post('/navers', NaverController.store);
+
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.create);
 
