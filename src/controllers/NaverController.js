e664969d27016a29cbd327988e749bb9a6419bb3
Gleider <gmdcampos@inf.ufpel.edu.br>
2020-08-02 14:55:26 -0300

    add index to find all navers and able to use filters

--- src/controllers/NaverController.js
+++ src/controllers/NaverController.js
@@ -1,6 +1,31 @@
 const Naver = require('../model/naver');
 
 module.exports = {
+  async index(req, res, next) {
+    try {
+      const { user_id } = req;
+      const { job_role, name, admission_time = 0 } = req.query;
+
+      const dt = new Date();
+      if (admission_time) {
+        dt.setFullYear(dt.getFullYear() - admission_time);
+      }
+      let query = {};
+      query = job_role ? { where: { job_role } } : {};
+      query = name ? { ...query, andWhere: { name } } : { ...query };
+
+      const navers = await Naver
+        .where('user_id', user_id)
+        .query(query)
+        .query('where', 'admission_date', '<=', dt)
+        .fetchAll({ require: false });
+
+      return res.status(200).json(navers);
+    } catch (error) {
+      return next(error.message);
+    }
+  },
+
   async store(req, res, next) {
     try {
       const {
@@ -12,6 +37,7 @@ module.exports = {
         return res.status(400)
           .json({ Error: 'Name, birthdate, admission date and job role are required' });
       }
+
       const user = await Naver.forge().save({
         user_id,
         name,
