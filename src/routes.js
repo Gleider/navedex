5d9c9a4732faafdd7f9bc9447258ca6f1c2e2884
Gleider <gmdcampos@inf.ufpel.edu.br>
2020-08-01 22:41:53 -0300

    add routes to signup and login

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const AuthController = require('./controllers/AuthController');
 const UserController = require('./controllers/UserController');
 
 const routes = express.Router();
 
+routes.post('/signup', AuthController.signup);
+routes.post('/login', AuthController.login);
+
 routes.get('/users', UserController.index);
+routes.post('/users', UserController.create);
 
 module.exports = routes;
