aa7a934d3beee221bdb23b7556f8ee0a34d841f0
Gleider <gmdcampos@inf.ufpel.edu.br>
2020-08-03 01:57:01 -0300

    changed user seed

--- src/database/seeds/1_users.js
+++ src/database/seeds/1_users.js
@@ -1,5 +1,4 @@
 const bcrypt = require('bcryptjs');
-const { v4: uuid } = require('uuid');
 
 const hash = bcrypt.hashSync('abc123', 10);
 
@@ -8,11 +7,9 @@ exports.seed = function (knex) {
   return knex('users').del()
     .then(() => knex('users').insert([
       {
-        id: uuid(),
+        id: '7df4d07a-3412-43a2-b36a-66dc49acf481',
         email: 'gleider.ec@gmail.com',
-        name: 'Gleider Mackedanz de Campos',
         password: hash,
-        birthdate: '1986-07-26',
       },
     ]));
 };
